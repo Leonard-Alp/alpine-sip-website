36cd0417a5f8a4061929215fc0afab9d114d5fd4
Leonard-Alp <leonard.hensel@web.de>
2026-03-23 22:37:38 +0100

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <main style={{ padding: "60px 20px", maxWidth: 900, margin: "0 auto" }}>
+      <h1 style={{ fontSize: 48 }}>
+        Das Gefühl von Skiwasser.<br />Als Marke gedacht.
+      </h1>
+
+      <p style={{ marginTop: 20, fontSize: 18, opacity: 0.8 }}>
+        Alpine Sip macht aus einem vertrauten Hüttengefühl ein
+        wiedererkennbares Lifestyle-Produkt.
+      </p>
+    </main>
+  );
+}
